17eb0687b68b972e13600687be3c6afeabbae323
23nith <user.name>
2022-05-18 17:52:35 +0800

    added axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const API_URL = 'http://localhost:3000/Login'
+
+function getAPIDATA() {
+  return axios.get(API_URL).then((response) => response.data)
+}
 
 function App() {
+  const [login, setLogin] = useState([])
+
+  // useEffect(() => {
+  //   let mounted = true;
+  //   getAPIDATA().then((items) => {
+  //     if(mounted){
+  //       setLogin(items);
+  //     }
+  //   });
+  //   return () => {mounted = false}
+  // }, [])
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
     </div>
   );
 }
